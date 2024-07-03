761642640e98809a3598541cbdd8e4d7ecf85c69
mohamedElnagar <mohamedsaid@affsquare.com>
2024-07-03 14:36:33 +0300

    create outgoing component

---
+++ src/app/outgoing/outgoing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outgoing',
+  standalone: true,
+  imports: [],
+  templateUrl: './outgoing.component.html',
+  styleUrl: './outgoing.component.scss'
+})
+export class OutgoingComponent {
+
+}
