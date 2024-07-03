093496ce526808583f69f1762a5c567d88470cb3
mohamedElnagar <mohamedsaid@affsquare.com>
2024-07-03 13:56:40 +0300

    update transactions page

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TransactionsService } from './transactions.service';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transactions',
   standalone: true,
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule, NgxPaginationModule, FormsModule],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.scss',
 })
@@ -17,16 +18,22 @@ export class TransactionsComponent {
   expenseCount: number = 1;
   p: number = 1;
   startIndex: number = 1;
-  searchDate: string = '2024-07-02';
+  searchDate: string = '2024-07-03';
   totalAmountPaid: number = 0;
   totalDailyExpense: number = 0;
   totalExistMoney: number = 0;
+  search: string = ' ';
 
-  constructor(private transactionsService: TransactionsService) {}
+  constructor(
+    private transactionsService: TransactionsService // private currencyPipe: CurrencyPipe
+  ) {}
+
   ngOnInit(): void {
     this.calcDailySales(this.p, this.searchDate);
   }
   calcDailySales(p: number, searchDate: string, searchTerm?: any): void {
+    console.log('ruuuuuuuuuun', searchTerm);
+    console.log('ruuuuuuuuuun', searchDate);
     this.transactionsService
       .calcDailySales(p, searchDate, searchTerm)
       .subscribe(
