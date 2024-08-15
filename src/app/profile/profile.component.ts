f2861eb5ab93c9250b151f8ac8efc41465291e80
mohamedElnagar <mohamedsaid@affsquare.com>
2024-08-15 16:29:46 +0300

    add tabs and page html strucure with css

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, faGear, faLock } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-
+  faUser = faUser;
+  faGear = faGear;
+  faLock = faLock;
 }
