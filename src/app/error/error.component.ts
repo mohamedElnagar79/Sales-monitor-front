56abbd950f4dae16a14194e472006b73f594ac16
mohamedElnagar <mohamedsaid@affsquare.com>
2024-09-10 11:20:27 +0300

    add error component

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,12 +1,35 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRefresh } from '@fortawesome/free-solid-svg-icons';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-error',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './error.component.html',
-  styleUrl: './error.component.scss'
+  styleUrl: './error.component.scss',
 })
 export class ErrorComponent {
-
+  faRefresh = faRefresh;
+  lastRoute: string = 'products';
+  previousUrl: string = '';
+  currentUrl: string = '';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private location: Location
+  ) {}
+  ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((queryParams) => {
+      console.log('queryyyy ', queryParams);
+      const previousRoute = queryParams['previousRoute'];
+      // Do something with the previous route information
+      console.log('Previous route:', previousRoute);
+    });
+  }
+  navigateBack() {
+    this.location.back();
+  }
 }
